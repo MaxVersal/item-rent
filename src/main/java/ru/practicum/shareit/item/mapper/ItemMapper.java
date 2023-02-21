52a6f8a4b88cd2c8cbbcf23e96e06a7eb2cc45d3
MaxVersal <m.stowell@mail.ru>
2023-02-21 21:41:48 +0300

    lombok and mapstruct conflicting solution

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,11 +1,16 @@
 package ru.practicum.shareit.item.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
 
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "available", source = "available")
+    @Mapping(target = "id", source = "id")
     ItemDto toDto(Item item);
 }

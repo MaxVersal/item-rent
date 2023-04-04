e67b219aff031b933c1d3fcb1e09963f9a80bb80
MaxVersal <m.stowell@mail.ru>
2023-04-04 16:59:55 +0300

    removing validation from server

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -11,7 +11,6 @@ import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
